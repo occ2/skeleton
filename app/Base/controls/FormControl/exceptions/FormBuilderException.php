7b305629d7dacf897afdafb2a5b456beacface0e
Milan Onderka <milan_onderka@occ2.cz>
2018-08-01 11:19:59 +0200

    namespace refactoring, annotation completitions

--- app/Base/controls/FormControl/exceptions/FormBuilderException.php
+++ app/Base/controls/FormControl/exceptions/FormBuilderException.php
@@ -1,11 +1,11 @@
 <?php
-namespace occ2\FormControl;
+namespace app\Base\controls\FormControl\exceptions;
 
 /**
  * FormBuilderException
  *
  * @author Milan Onderka <milan_onderka@occ2.cz>
- * @version 1.0.0
+ * @version 1.1.0
  */
 class FormBuilderException extends \Exception
 {
