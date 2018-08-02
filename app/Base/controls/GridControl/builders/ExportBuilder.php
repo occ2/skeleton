67d8f14d7cb0b521b10e949f700dc6efa7a179c8
Milan Onderka <milan_onderka@occ2.cz>
2018-08-02 10:41:24 +0200

    namespace refactoring, annotation completitions

--- app/Base/controls/GridControl/builders/ExportBuilder.php
+++ app/Base/controls/GridControl/builders/ExportBuilder.php
@@ -1,26 +1,54 @@
 <?php
-namespace occ2\GridControl;
+namespace app\Base\controls\GridControl\builders;
 
+use app\Base\controls\GridControl\traits\TCallbacks;
+use app\Base\controls\GridControl\builders\IAdditionalGridBuilder;
+use app\Base\controls\GridControl\GridControl;
+use app\Base\controls\GridControl\configurators\GridConfig;
+use app\Base\controls\GridControl\exceptions\GridBuilderException;
+use app\Base\controls\GridControl\builders\GridBuilder;
 use Ublaboo\DataGrid\DataGrid;
-use Nette\Utils\ArrayHash;
 use Ublaboo\DataGrid\Export\Export;
+use Nette\Utils\ArrayHash;
 
 /**
  * ExportBuilder
  *
  * @author Milan Onderka <milan_onderka@occ2.cz>
- * @version 1.0.0
+ * @version 1.1.0
  */
 class ExportBuilder implements IAdditionalGridBuilder
 {
     use TCallbacks;
-    
+
+    /**
+     * @var DataGrid
+     */
     protected $grid;
+
+    /**
+     * @var GridConfig
+     */
     protected $configurator;
+
+    /**
+     * @var GridControl
+     */
     protected $object;
+
+    /**
+     * @var array
+     */
     protected $callbacks;
-    
-    public function __construct($object, DataGrid $grid, GridConfig $configurator, ArrayHash $callbacks)
+
+    /**
+     * @param GridControl $object
+     * @param DataGrid $grid
+     * @param GridConfig $configurator
+     * @param array $callbacks
+     * @return void
+     */
+    public function __construct(GridControl $object, DataGrid $grid, GridConfig $configurator, array $callbacks)
     {
         $this->object = $object;
         $this->grid = $grid;
@@ -28,21 +56,38 @@ class ExportBuilder implements IAdditionalGridBuilder
         $this->callbacks = $callbacks;
         return;
     }
-    
+
+    /**
+     * build exports
+     * @return void
+     */
     public function build()
     {
         return $this->addExports($this->grid);
     }
-    
+
+    /**
+     * add one export
+     * @param DataGrid $grid
+     * @return void
+     */
     protected function addExports(DataGrid $grid)
     {
         $configs = $this->configurator->getExport(true);
         foreach ($configs as $config) {
             $this->addExport($grid, $config);
         }
+        return;
     }
-    
-    protected function addExport(DataGrid $grid, ArrayHash $config)
+
+    /**
+     * add one export
+     * @param DataGrid $grid
+     * @param ArrayHash $config
+     * @return Export
+     * @throws GridBuilderException
+     */
+    protected function addExport(DataGrid $grid, ArrayHash $config): Export
     {
         if (!isset($config->name) || empty($config->name)) {
             throw new GridBuilderException("ERROR: Export name must be set", GridBuilderException::UNDEFINED_EXPORT_NAME);
@@ -54,8 +99,15 @@ class ExportBuilder implements IAdditionalGridBuilder
             return $this->addCSVExport($grid, $config, $filtered);
         }
     }
-    
-    protected function addCallbackExport(DataGrid $grid, ArrayHash $config, $filtered)
+
+    /**
+     * add callback export
+     * @param DataGrid $grid
+     * @param ArrayHash $config
+     * @param bool $filtered
+     * @return Export
+     */
+    protected function addCallbackExport(DataGrid $grid, ArrayHash $config, bool $filtered):Export
     {
         $t = $this;
         $export = $grid->addExportCallback(
@@ -68,8 +120,15 @@ class ExportBuilder implements IAdditionalGridBuilder
         $this->setupExport($export, $config);
         return $export;
     }
-    
-    protected function addCSVExport(DataGrid $grid, ArrayHash $config, $filtered)
+
+    /**
+     * add CSV export
+     * @param DataGrid $grid
+     * @param ArrayHash $config
+     * @param bool $filtered
+     * @return Export
+     */
+    protected function addCSVExport(DataGrid $grid, ArrayHash $config, bool $filtered): Export
     {
         if ($filtered) {
             $export = $grid->addExportCsvFiltered(
@@ -91,7 +150,13 @@ class ExportBuilder implements IAdditionalGridBuilder
         $this->setupExport($export, $config);
         return $export;
     }
-    
+
+    /**
+     * setup export
+     * @param Export $export
+     * @param ArrayHash $config
+     * @return void
+     */
     protected function setupExport(Export $export, ArrayHash $config)
     {
         !isset($config->class) ?: $export->setClass($config->class);
