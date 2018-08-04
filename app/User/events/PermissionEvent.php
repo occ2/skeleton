4bd34928530a17666974d6b04f1f103da880375d
Milan Onderka <milan_onderka@occ2.cz>
2018-08-04 22:51:17 +0200

    add annotation based automatic acl tests

---
+++ app/User/events/PermissionEvent.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\User\events;
+
+use app\Base\events\Event;
+
+/**
+ * PermissionEvent
+ *
+ * @author Milan Onderka <milan_onderka@occ2.cz>
+ * @version 1.1.0
+ */
+class PermissionEvent extends Event
+{
+    
+}
