5e902cbd63a368011b3ee9781d8b95729b601eda
Milan Onderka <milan_onderka@occ2.cz>
2018-08-04 23:25:16 +0200

    namespace refactoring

--- app/User/controls/factories/ISignInForm.php
+++ app/User/controls/factories/ISignInForm.php
@@ -1,15 +1,15 @@
 <?php
-namespace occ2\inventar\User\controls\forms;
+namespace app\User\controls\factories;
 
 /**
  * ISignInForm factory inteface
  * @author Milan Onderka <milan_onderka@occ2.cz>
- * @version 1.0.0
+ * @version 1.1.0
  */
 interface ISignInForm
 {
     /**
-     * @return \occ2\inventar\User\controls\forms\SignInForm
+     * @return \app\User\controls\forms\SignInForm
      */
     public function create();
 }
